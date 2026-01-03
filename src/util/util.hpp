2e34b3afff932d144f1339f433b7b4ab79f72878
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2026-01-03 14:34:24 +0545

    add: array and vector element contain test function

--- src/util/util.hpp
+++ src/util/util.hpp
@@ -30,3 +30,37 @@ static void PrintVec(const Vector<T>& vec, const String& title, const String& vs
     std::cout << END_LINE_CMD;
     Seperator();
 }
+
+/*
+Check if the element exists on pointer array,
+on success returns the index else returns -1
+*/
+template<typename T>
+static int ArrContains(const T *arr, const uint count, const T& c)
+{
+    for (uint i=0;i<count;++i)
+    {
+        const auto& item = *(arr + i);
+        if (item == c) 
+            return i;
+    }
+
+    return -1;
+}
+
+/*
+Check if the element exists on vector,
+on success returns the index else returns -1
+*/
+template<typename T>
+static int VecContains(const Vector<T>& vec, const T& c)
+{
+    for (uint i=0;i<vec.size();++i)
+    {
+        const auto& item = vec[i];
+        if (item == c) 
+            return i;
+    }
+
+    return -1;
+}
