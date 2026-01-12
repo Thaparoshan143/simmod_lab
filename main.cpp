4213ca144d920f3d890e50286cda442d57276c3c
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2026-01-12 21:08:12 +0545

    add: basic impl of lab-2 (no. 5 left to be done)

--- main.cpp
+++ main.cpp
@@ -3,5 +3,7 @@
 // #include <lab1/ex1.cpp>
 // #include <lab1/ex1-r.cpp>
 // #include <lab1/ex1-i.cpp>
-#include <lab1/ex1-mi.cpp>
+// #include <lab1/ex1-mi.cpp>
 // #include <lab1/ex1-mi-r.cpp>
+
+#include <lab2/ex1.cpp>
