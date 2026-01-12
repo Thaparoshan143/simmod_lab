0b8f7f13c04a921dc538d32acc8458b162e5bcd4
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2026-01-12 21:04:15 +0545

    add: some more helper functions

--- src/util/util.hpp
+++ src/util/util.hpp
@@ -64,3 +64,170 @@ static int VecContains(const Vector<T>& vec, const T& c)
 
     return -1;
 }
+
+/*
+Multiply the Vector by single scalar (one scalar multiple to all element in vector)
+# Warning: original vector is modified with result.
+*/
+// template<typename T, typename U>
+// static void VecScalMul(Vector<T>& vec, const U& sca)
+// {
+//     for (auto& item: vec)
+//     {
+//         item *= sca;
+//     }
+// }
+
+/*
+Multiply the Vector by single scalar (one scalar multiple to all element in vector)
+returns new vector with result
+*/
+template<typename T, typename U>
+static Vector<T> VecScalMul(const Vector<T>& vec, const U& sca)
+{
+    Vector<T> res {};
+    res.reserve(vec.size());
+    for (const auto& item: vec)
+    {
+        res.push_back(item * sca);
+    }
+    return res;
+}
+
+/*
+Multiply the Vector by complex scalar (one complex valued scalar multiple to all element in Complex vector)
+# Warning: original vector is modified with result.
+*/
+template<typename T>
+static void VecScalMul(Vector<Complex<T>> vec, const Complex<T>& sca)
+{
+    for (auto& item: vec)
+    {
+        item *= sca;
+    }
+}
+
+/*
+Callback for each vector element passed as arg for some operation
+# Warning: original vector is modified with result.
+*/
+// template<typename VecType, typename Op>
+// static void VecOpCb(VecType& vec, Op cb)
+// {
+//     for (auto& item: vec)
+//     {
+//         item = cb(item);
+//     }
+// }
+
+/*
+Callback for each vector element passed as arg for some operation (lambda)
+return is new vector with result.. 
+*/
+template<typename T, typename Op>
+static Vector<T> VecOpCb(const Vector<T>& vec, Op cb)
+{
+    Vector<T> res {};
+    res.reserve(vec.size());
+
+    for (const auto& item: vec)
+    {
+        res.push_back((cb(item)));
+    }
+    return res;
+}
+
+/*
+Callback for each vector element passed as arg for some operation (lambda)
+return is new vector with result.. stored in complex form
+*/
+template<typename T, typename Op>
+static Vector<Complex<T>> VecOpCbComplex(const Vector<T>& vec, Op cb)
+{
+    Vector<Complex<T>> res {};
+    res.reserve(vec.size());
+
+    for (const auto& item: vec)
+    {
+        res.push_back((cb(item)));
+    }
+    return res;
+}
+
+template<typename T>
+static Vector<Complex<T>> RealToComplex(const Vector<T>& vec)
+{
+    Vector<Complex<T>> res {};
+    res.reserve(vec.size());
+
+    for (const auto& item: vec)
+    {
+        res.push_back(Complex<T>(item, 0));
+    }
+    return res;
+}
+
+/*
+Callback for dual vector where each element of both vector (same index) passed as arg for some operation in callback (lambda)
+return the result 
+*/
+template<typename T, typename Op>
+static Vector<T> DualVecOpCb(const Vector<T>& vec1, const Vector<T>& vec2, Op cb)
+{
+    if (vec1.size() != vec2.size())
+    {
+        std::cerr << "Vector size must be same!" << std::endl;
+        exit(-1);
+    }
+
+    Vector<T> res {};
+    res.reserve(vec1.size());
+
+    for (uint i=0;i<vec1.size();++i)
+    {
+        res.push_back((cb(vec1[i], vec2[i])));
+    }
+
+    return res;
+}
+
+/*
+function for calculation of Complex number magnitudes
+returns new vector with magnitude
+*/
+template<typename T>
+static Vector<T> GetComplexVecMag(const Vector<Complex<T>>& vec)
+{
+    Vector<T> res {};
+    res.reserve(vec.size());
+
+    for (const auto& item: vec)
+    {
+        res.push_back(std::abs(item));
+    }
+
+    return res;
+}
+
+#define DEGREE_MULTIPLE 180
+#define PI 3.1415
+/*
+function for calculation of Complex number Phase
+returns new vector with magnitude
+*/
+template<typename T>
+static Vector<T> GetComplexVecPhase(const Vector<Complex<T>>& vec, const bool isDegree = false)
+{
+    Vector<T> res {};
+    res.reserve(vec.size());
+
+    T theta {};
+
+    for (const Complex<T>& item: vec)
+    {
+        theta = atan2(item.imag(), item.real());
+        res.push_back(isDegree ? theta * DEGREE_MULTIPLE / PI : theta);
+    }
+
+    return res;
+}
